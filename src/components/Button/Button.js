c50d0f7ee84f506e8886279125b7166cd9a1036c
Jessica Sena <jessisena@gmail.com>
2018-12-17 14:13:18 +0100

    Add storybook to the project, and new components

---
+++ src/components/Button/Button.js
@@ -0,0 +1,17 @@
+//@flow
+import React from "react";
+import { Button as ButtonSUI } from "semantic-ui-react";
+
+export default function Button(props) {
+	console.log(props);
+
+	const buttonProps = {
+		color: "yellow",
+		onClick: ()=>console.log("click"),
+		...props
+	};
+
+	return (<ButtonSUI primary {...buttonProps}></ButtonSUI>);
+
+}
+
