be90561da1ac3be7b50e075fca11829b1b145a93
Jess <jessisena@gmail.com>
2019-03-28 13:16:37 +0100

    Canvis al travis per fer build automatic

--- dist/index.js
+++
@@ -1,6 +0,0 @@
-import Map from "./components/Map/Map";
-import Button from "./components/Button/Button";
-import NavButtons from "./components/NavButtons/NavButtons";
-import LogoICGC from "./components/LogoICGC/LogoICGC";
-
-export { Map, Button, NavButtons, LogoICGC };
