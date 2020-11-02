421b5332676fb67bba3d715967b3c1f7594df18a
Wladimir Szczerban <bolosig@gmail.com>
2020-11-02 13:50:10 +0100

    build 0.3.1

--- dist/commonjs/components/Button/Button.js
+++ dist/commonjs/components/Button/Button.js
@@ -3,19 +3,28 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.default = Button;
+exports.default = exports.Button = void 0;
 
 var _react = _interopRequireDefault(require("react"));
+
+var _propTypes = _interopRequireDefault(require("prop-types"));
 
 var _semanticUiReact = require("semantic-ui-react");
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function Button(props) {
-  const buttonProps = {
-    color: "yellow",
-    onClick: () => console.log("click"),
-    ...props
-  };
-  return /*#__PURE__*/_react.default.createElement(_semanticUiReact.Button, buttonProps);
-}
+const Button = props => /*#__PURE__*/_react.default.createElement(_semanticUiReact.Button, props);
+
+exports.Button = Button;
+var _default = Button;
+exports.default = _default;
+Button.propTypes = {
+  color: _propTypes.default.string,
+  content: _propTypes.default.string,
+  onClick: _propTypes.default.func
+};
+Button.defaultProps = {
+  color: "yellow",
+  content: "",
+  onClick: () => console.log("click")
+};
--- dist/es/components/Button/Button.js
+++ dist/es/components/Button/Button.js
@@ -1,10 +1,15 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { Button as ButtonSUI } from "semantic-ui-react";
-export default function Button(props) {
-  const buttonProps = {
-    color: "yellow",
-    onClick: () => console.log("click"),
-    ...props
-  };
-  return /*#__PURE__*/React.createElement(ButtonSUI, buttonProps);
-}
+export const Button = props => /*#__PURE__*/React.createElement(ButtonSUI, props);
+export default Button;
+Button.propTypes = {
+  color: PropTypes.string,
+  content: PropTypes.string,
+  onClick: PropTypes.func
+};
+Button.defaultProps = {
+  color: "yellow",
+  content: "",
+  onClick: () => console.log("click")
+};
